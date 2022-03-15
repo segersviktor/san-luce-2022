f1ce5381e54a62ccede2ec4b5409d38f10284e5d
Lennert Slabbinck <lennert.slabbinck@hotmail.com>
2022-03-15 21:44:28 +0100

    Added boilerplate scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import React from 'react';
+import App from 'next/app';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/app.scss';
+
+class MyApp extends App {
+    static async getInitialProps(appContext) {
+        // calls page's `getInitialProps` and fills `appProps.pageProps`
+        const appProps = await App.getInitialProps(appContext);
+
+        return {...appProps};
+    }
+
+    render() {
+        const {Component, pageProps} = this.props;
+
+        return (
+            <Component {...pageProps} />
+        );
+    }
 }
 
-export default MyApp
+export default MyApp;
